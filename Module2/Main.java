0d86c551298c8cf8b865b1d7c4221d5b37b25cff
A-CHINMAY <a.chinmay2005@gmail.com>
2024-10-18 21:02:52 +0530

    updated files

--- Module2/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    // Outer loop.
-    for (int i = 1; i <= 2; i++) {
-      System.out.println("Outer: " + i); // Executes 2 times
-      }
-    } 
-  }
-
