8f6f61afb1a2ff2d93575151711eba1544812e34
A-CHINMAY <a.chinmay2005@gmail.com>
2025-01-22 15:44:45 +0530

    updated

--- Module5/Multithreading/JoinMeth.java
+++ Module5/Multithreading/JoinMeth.java
@@ -1,10 +1,9 @@
 package Module5.Multithreading;
 
-class MyRunnable implements Runnable {
+class MyThread extends Thread {
     public void run() {
         for (int i = 0; i < 2; i++) {
             try {
-                // Sleeping the thread for 300 milliseconds
                 Thread.sleep(300);
                 System.out.println("The current thread name is: " + Thread.currentThread().getName());
             } catch (InterruptedException e) {
@@ -17,28 +16,26 @@ class MyRunnable implements Runnable {
 
 public class JoinMeth {
     public static void main(String[] args) {
-        // Creating a Runnable object
-        MyRunnable myRunnable = new MyRunnable();
+     
+        MyThread th1 = new MyThread();
+        MyThread th2 = new MyThread();
+        MyThread th3 = new MyThread();
 
-        // Creating 3 threads and passing the Runnable object to each
-        Thread th1 = new Thread(myRunnable);
-        Thread th2 = new Thread(myRunnable);
-        Thread th3 = new Thread(myRunnable);
-
-        th1.start(); // Start thread th1
+        th1.start();
         try {
-            th1.join(); // Wait for thread th1 to finish before starting th2
+            th1.join();
         } catch (InterruptedException e) {
             System.out.println("Exception caught: " + e);
         }
 
-        th2.start(); // Start thread th2
+        th2.start(); 
         try {
-            th2.join(); // Wait for thread th2 to finish before starting th3
+            th2.join(); 
         } catch (InterruptedException e) {
             System.out.println("Exception caught: " + e);
         }
 
-        th3.start(); // Start thread th3
+        th3.start(); 
     }
 }
+
